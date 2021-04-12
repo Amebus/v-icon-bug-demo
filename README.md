e5a66b9ae5527e67b2d58ebb045d0618b52f4a53
Federico Ceruti <federico.ceruti@elfo.net>
2021-04-12 12:24:06 +0200

    v-icon issue

--- README.md
+++ README.md
@@ -1,26 +1,25 @@
 # Demo to Reproduce Vuetify Bug
 
-See https://github.com/vuetifyjs/vuetify/issues/13163
+See https://github.com/vuetifyjs/vuetify/issues/13163 and https://github.com/vuetifyjs/vuetify/issues/12962
 
 ## Project setup
-```
+
+```cmd
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```cmd
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```cmd
 npm run build
 ```
 
-### Run your unit tests
-```
-npm run test:unit
-```
-
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,31 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <v-btn @click="onClickElDocumentCreateElement">el: document.createElement('div')</v-btn>
+      <v-btn @click="onClickElString">el: 'div'</v-btn>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import Vue from 'vue';
 
 export default {
   components: {
     HelloWorld,
   },
+   methods: {
+    onClickElDocumentCreateElement: function() {
+      new (Vue.extend(HelloWorld))({
+        el: document.createElement('div')
+      });
+    },
+    onClickElString: function() {
+      new (Vue.extend(HelloWorld))({
+        el: 'div'
+      });
+    }
+  }
 };
 </script>
